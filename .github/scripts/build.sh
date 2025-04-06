68cba310763b6b7a1506eb601fdd6a18982f38db
kolibritask <kolibriexample@proton.me>
2025-04-06 10:30:53 +0200

    Adds cicd flow

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,6 +1,6 @@
 # This script is just an example running the standard build
 # It can be customized (and preferably changed to python)
-/opt/Unity/Editor/Unity \
+unity-editor \
     -batchmode \
     -nographics \
     -quit \
